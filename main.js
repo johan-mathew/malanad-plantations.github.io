45c3733a5e453ddca59ad1ce1c79cb77f55e6bdc
Johan K Mathew <31700508+johan-mathew@users.noreply.github.com>
2021-01-24 19:07:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+// The debounce function receives our function as a parameter
+const debounce = (fn) => {
+
+    // This holds the requestAnimationFrame reference, so we can cancel it if we wish
+    let frame;
+  
+    // The debounce function returns a new function that can receive a variable number of arguments
+    return (...params) => {
+      
+      // If the frame variable has been defined, clear it now, and queue for next frame
+      if (frame) { 
+        cancelAnimationFrame(frame);
+      }
+  
+      // Queue our function call for the next frame
+      frame = requestAnimationFrame(() => {
+        
+        // Call our function and pass any params we received
+        fn(...params);
+      });
+  
+    } 
+  };
+  
+  
+  // Reads out the scroll position and stores it in the data attribute
+  // so we can use it in our stylesheets
+  const storeScroll = () => {
+    document.documentElement.dataset.scroll = window.scrollY;
+  }
+  
+  // Listen for new scroll events, here we debounce our `storeScroll` function
+  document.addEventListener('scroll', debounce(storeScroll), { passive: true });
+  
+  // Update scroll position for first time
+  storeScroll();
+
+function openSidebar(){
+  document.getElementById("sidebar").style.right = 0;
+}
+
+function closeSidebar(){
+  document.getElementById("sidebar").style.right = "-100vw";
+}
+
+function viewAnswer(click_id) {
+  var x = document.getElementById("a-"+click_id);
+  var plus = document.querySelector("#"+ click_id + " .faq-plus");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    plus.innerHTML = "-";
+  } else {
+    x.style.display = "none";
+    plus.innerHTML = "+";
+  }
+}
+  var swiper = new Swiper('#header-carousel', {
+    spaceBetween: 0,
+    pagination: {
+      el: '#header-pagination',
+      clickable: true,
+    },
+  });
+
+  var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 0,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
